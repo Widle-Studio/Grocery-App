ab249a3e00cc21bd5a9ef36644c7b7604e97828e
Bhavik Limani <limanibhavik@gmail.com>
2022-11-07 11:52:17 +0530

    gitignore

--- f_groceries/ios/Flutter/flutter_export_environment.sh
+++ f_groceries/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Suyog Gaire\Grocery-App\f_groceries"
+export "FLUTTER_ROOT=/Users/bhaviklimani/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/bhaviklimani/Project/Grocery-App/f_groceries"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
